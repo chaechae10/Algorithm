b630b1babccad70950a19be3b3637828213abf12
CHAEYEON KIM <gimchaeyeon57@gmail.com>
2024-09-23 16:08:39 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 412 ms, Memory: 40768 KB -BaekjoonHub

--- 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
@@ -1,22 +1,23 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-
+import java.io.IOException;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        StringBuilder sb = new StringBuilder();
         int T = Integer.parseInt(br.readLine());
 
-        for(int i = 0; i < T; i++){
-            String[] words = br.readLine().split(" ");
-            
-            for(String word:words){
-                StringBuilder sb = new StringBuilder(word);
-                System.out.println(sb.reverse().toString());
+        for(int t = 1; t <= T; t++) {
+            StringTokenizer st = new StringTokenizer(br.readLine()," ");
+            while(st.hasMoreTokens()) {
+                StringBuilder sb2 = new StringBuilder(st.nextToken());
+                sb.append(sb2.reverse()).append(" ");
             }
-            }
+            sb.append('\n');
         }
+        System.out.println(sb);
     }
-
+}
